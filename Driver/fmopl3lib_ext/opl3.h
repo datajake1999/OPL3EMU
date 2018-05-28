1758cb739f27c2f6e0c57dd5ff5cde62bc0a56fb
datajake1999 <datajake@gmail.com>
2018-05-28 03:31:40 -0700

    Updated Nuked OPL3 to 1.8 in extended Doom driver.

--- Driver/fmopl3lib_ext/opl3.h
+++ Driver/fmopl3lib_ext/opl3.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2013-2017 Alexey Khokholov (Nuke.YKT)
+// Copyright (C) 2013-2018 Alexey Khokholov (Nuke.YKT)
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -15,22 +15,24 @@
 //  Nuked OPL3 emulator.
 //  Thanks:
 //      MAME Development Team(Jarek Burczynski, Tatsuyuki Satoh):
-//          Feedback calculation information.
+//          Feedback and Rhythm part calculation information.
 //      forums.submarine.org.uk(carbon14, opl3):
 //          Tremolo and phase generator calculation information.
 //      OPLx decapsulated(Matthew Gambrell, Olli Niemitalo):
 //          OPL2 ROMs.
+//      siliconpr0n.org(John McMaster, digshadow):
+//          YMF262 and VRC VII decaps and die shots.
 //
-// version: 1.7.4
+// version: 1.8
 //
 
 #ifndef OPL_OPL3_H
 #define OPL_OPL3_H
 
-#include <stdint.h>
+#include <inttypes.h>
 
 #define OPL_WRITEBUF_SIZE   1024
-#define OPL_WRITEBUF_DELAY  1
+#define OPL_WRITEBUF_DELAY  2
 
 typedef uintptr_t       Bitu;
 typedef intptr_t        Bits;
@@ -73,7 +75,10 @@ struct _opl3_slot {
     Bit8u reg_rr;
     Bit8u reg_wf;
     Bit8u key;
+    Bit32u pg_reset;
     Bit32u pg_phase;
+    Bit16u pg_phase_out;
+    Bit8u slot_num;
 };
 
 struct _opl3_channel {
@@ -89,6 +94,7 @@ struct _opl3_channel {
     Bit8u alg;
     Bit8u ksv;
     Bit16u cha, chb;
+    Bit8u ch_num;
     Bit32s leftpan;
     Bit32s rightpan;
 };
@@ -103,6 +109,10 @@ struct _opl3_chip {
     opl3_channel channel[18];
     opl3_slot slot[36];
     Bit16u timer;
+    Bit64u eg_timer;
+    Bit8u eg_timerrem;
+    Bit8u eg_state;
+    Bit8u eg_add;
     Bit8u newm;
     Bit8u nts;
     Bit8u rhy;
@@ -114,7 +124,12 @@ struct _opl3_chip {
     Bit32u noise;
     Bit16s zeromod;
     Bit32s mixbuff[2];
-
+    Bit8u rm_hh_bit2;
+    Bit8u rm_hh_bit3;
+    Bit8u rm_hh_bit7;
+    Bit8u rm_hh_bit8;
+    Bit8u rm_tc_bit3;
+    Bit8u rm_tc_bit5;
     Bit8u stereoext;
     //OPL3L
     Bit32s rateratio;
