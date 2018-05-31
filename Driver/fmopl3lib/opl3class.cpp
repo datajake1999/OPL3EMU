ee30166992678d4a545b2aac1f4239a1240e18e3
datajake1999 <datajake@gmail.com>
2018-05-30 20:06:45 -0700

    Indentation fix.

--- Driver/fmopl3lib/opl3class.cpp
+++ Driver/fmopl3lib/opl3class.cpp
@@ -15,12 +15,12 @@
 #include <string.h>
 #include "opl3class.h"
 
-	char *core;
-	char *silence;
-	char *hwsupport;
-	char *wavwrite;
-	char *vgmlog;
-	char *vgmloop;
+char *core;
+char *silence;
+char *hwsupport;
+char *wavwrite;
+char *vgmlog;
+char *vgmloop;
 const Bit64u lat = (50 * 49716) / 1000;
 
 
@@ -33,193 +33,193 @@ int opl3class::fm_init(unsigned int rate) {
 	vgmloop = getenv("VGMLOOP");
 	if (core)
 	{
-if (strstr(core, "-dbcompat"))
-{
-    adlib_init(rate);
-}
-if (strstr(core, "-dbfast"))
-{
-    chip2.Init(rate);
-}
-if (strstr(core, "-mame"))
-{
-    chip3 = ymf262_init(49716*288, rate);
+		if (strstr(core, "-dbcompat"))
+		{
+			adlib_init(rate);
 		}
-}
-else
-{
-    OPL3_Reset(&chip, rate);
-}
+		if (strstr(core, "-dbfast"))
+		{
+			chip2.Init(rate);
+		}
+		if (strstr(core, "-mame"))
+		{
+			chip3 = ymf262_init(49716*288, rate);
+		}
+	}
+	else
+	{
+		OPL3_Reset(&chip, rate);
+	}
 	if (hwsupport)
 	{
 		if (strstr(hwsupport, "-on"))
 		{
-OPL_Hardware_Detection();
-OPL_HW_Init();
+			OPL_Hardware_Detection();
+			OPL_HW_Init();
 		}
 	}
 	if (wavwrite)
 	{
 		if (strstr(wavwrite, "-on"))
 		{
-WavFileOpen(rate);
+			WavFileOpen(rate);
 		}
 	}
 	if (vgmlog)
 	{
 		if (strstr(vgmlog, "-on"))
 		{
-VGMLog_Init(rate);
+			VGMLog_Init(rate);
 		}
 	}
 	if (vgmloop)
 	{
 		if (strstr(vgmloop, "-on"))
 		{
-VGMLog_MarkLoopStartNow();
+			VGMLog_MarkLoopStartNow();
 		}
 	}
 
-    memset(command,0,sizeof(command));
-    memset(time, 0, sizeof(time));
-    counter = 0;
-    lastwrite = 0;
-    strpos = 0;
-    endpos = 0;
+	memset(command,0,sizeof(command));
+	memset(time, 0, sizeof(time));
+	counter = 0;
+	lastwrite = 0;
+	strpos = 0;
+	endpos = 0;
 
 	return 1;
 }
 
 void opl3class::fm_writereg(unsigned short reg, unsigned char data) {
-    command[endpos % 8192][0] = reg;
-    command[endpos % 8192][1] = data;
-    Bit64u t1 = lastwrite + 2;
-    Bit64u t2 = counter + lat;
-    if (t2 > t1)
-    {
-        t1 = t2;
-    }
-    time[endpos % 8192] = t1;
-    lastwrite = t1;
-    endpos = (endpos + 1) % 8192;
+	command[endpos % 8192][0] = reg;
+	command[endpos % 8192][1] = data;
+	Bit64u t1 = lastwrite + 2;
+	Bit64u t2 = counter + lat;
+	if (t2 > t1)
+	{
+		t1 = t2;
+	}
+	time[endpos % 8192] = t1;
+	lastwrite = t1;
+	endpos = (endpos + 1) % 8192;
 }
 
 void opl3class::fm_generate(signed short *buffer, unsigned int len) {
-    for (unsigned int i = 0; i < len; i++)
-    {
-        while (strpos != endpos && time[strpos] < counter)
-        {
-	if (core)
+	for (unsigned int i = 0; i < len; i++)
 	{
-if (strstr(core, "-dbcompat"))
-{
-	if (silence)
-	{
-		if (strstr(silence, "-on"))
+		while (strpos != endpos && time[strpos] < counter)
 		{
-            adlib_write(0x00, 0x00);
+			if (core)
+			{
+				if (strstr(core, "-dbcompat"))
+				{
+					if (silence)
+					{
+						if (strstr(silence, "-on"))
+						{
+							adlib_write(0x00, 0x00);
+						}
+					}
+					else
+					{
+						adlib_write(command[strpos][0], command[strpos][1]);
+					}
+				}
+				if (strstr(core, "-dbfast"))
+				{
+					if (silence)
+					{
+						if (strstr(silence, "-on"))
+						{
+							chip2.WriteReg(0x00, 0x00);
+						}
+					}
+					else
+					{
+						chip2.WriteReg(command[strpos][0], command[strpos][1]);
+					}
+				}
+				if (strstr(core, "-mame"))
+				{
+					if (silence)
+					{
+						if (strstr(silence, "-on"))
+						{
+							ymf262_write_reg(chip3, 0x00, 0x00);
+						}
+					}
+					else
+					{
+						ymf262_write_reg(chip3, command[strpos][0], command[strpos][1]);
+					}
+				}
+			}
+			else
+			{
+				if (silence)
+				{
+					if (strstr(silence, "-on"))
+					{
+						OPL3_WriteReg(&chip, 0x00, 0x00);
+					}
+				}
+				else
+				{
+					OPL3_WriteReg(&chip, command[strpos][0], command[strpos][1]);
+				}
+			}
+			if (hwsupport)
+			{
+				if (strstr(hwsupport, "-on"))
+				{
+					OPL_HW_WriteReg(command[strpos][0], command[strpos][1]);
+				}
+			}
+			if (vgmlog)
+			{
+				if (strstr(vgmlog, "-on"))
+				{
+					VGMLog_CmdWrite((0x5E | (command[strpos][0]>>8)), (BYTE)command[strpos][0], command[strpos][1]);
+				}
+			}
+			strpos = (strpos + 1) % 8192;
 		}
-	}
-else
-{
-            adlib_write(command[strpos][0], command[strpos][1]);
-}
-}
-if (strstr(core, "-dbfast"))
-{
-	if (silence)
-	{
-		if (strstr(silence, "-on"))
+		if (core)
 		{
-            chip2.WriteReg(0x00, 0x00);
+			if (strstr(core, "-dbcompat"))
+			{
+				adlib_getsample((Bit16s*)buffer, 1);
+			}
+			if (strstr(core, "-dbfast"))
+			{
+				chip2.Generate(buffer, 1);
+			}
+			if (strstr(core, "-mame"))
+			{
+				ymf262_update_one(chip3, buffer, 1);
+			}
 		}
-	}
-else
-{
-            chip2.WriteReg(command[strpos][0], command[strpos][1]);
-}
-}
-if (strstr(core, "-mame"))
-{
-	if (silence)
-	{
-		if (strstr(silence, "-on"))
+		else
 		{
-            ymf262_write_reg(chip3, 0x00, 0x00);
+			OPL3_GenerateStream(&chip, (Bit16s*)buffer, 1);
 		}
-	}
-else
-{
-            ymf262_write_reg(chip3, command[strpos][0], command[strpos][1]);
-}
-}
-}
-else
-{
-	if (silence)
-	{
-		if (strstr(silence, "-on"))
+		if (wavwrite)
 		{
-            OPL3_WriteReg(&chip, 0x00, 0x00);
+			if (strstr(wavwrite, "-on"))
+			{
+				WavFileWrite(buffer, 1);
+			}
 		}
-	}
-else
-{
-            OPL3_WriteReg(&chip, command[strpos][0], command[strpos][1]);
-}
-}
-	if (hwsupport)
-	{
-		if (strstr(hwsupport, "-on"))
+		if (vgmlog)
 		{
-OPL_HW_WriteReg(command[strpos][0], command[strpos][1]);
+			if (strstr(vgmlog, "-on"))
+			{
+				VGMLog_IncrementSamples(1);
+			}
 		}
+		buffer += 2;
+		counter++;
 	}
-	if (vgmlog)
-	{
-		if (strstr(vgmlog, "-on"))
-		{
-VGMLog_CmdWrite((0x5E | (command[strpos][0]>>8)), (BYTE)command[strpos][0], command[strpos][1]);
-		}
-	}
-            strpos = (strpos + 1) % 8192;
-        }
-	if (core)
-	{
-if (strstr(core, "-dbcompat"))
-{
-        adlib_getsample((Bit16s*)buffer, 1);
-}
-if (strstr(core, "-dbfast"))
-{
-        chip2.Generate(buffer, 1);
-}
-if (strstr(core, "-mame"))
-{
-        ymf262_update_one(chip3, buffer, 1);
-}
-}
-else
-{
-        OPL3_GenerateStream(&chip, (Bit16s*)buffer, 1);
-}
-	if (wavwrite)
-	{
-		if (strstr(wavwrite, "-on"))
-		{
-WavFileWrite(buffer, 1);
-		}
-	}
-	if (vgmlog)
-	{
-		if (strstr(vgmlog, "-on"))
-		{
-VGMLog_IncrementSamples(1);
-		}
-	}
-        buffer += 2;
-        counter++;
-    }
 }
 
 void opl3class::fm_close() {
@@ -227,21 +227,21 @@ void opl3class::fm_close() {
 	{
 		if (strstr(hwsupport, "-on"))
 		{
-OPL_HW_Close();
+			OPL_HW_Close();
 		}
 	}
 	if (wavwrite)
 	{
 		if (strstr(wavwrite, "-on"))
 		{
-WavFileClose();
+			WavFileClose();
 		}
 	}
 	if (vgmlog)
 	{
 		if (strstr(vgmlog, "-on"))
 		{
-VGMLog_Close();
+			VGMLog_Close();
 		}
 	}
 }
