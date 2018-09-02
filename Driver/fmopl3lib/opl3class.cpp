026c4c85776449a62db01669e8796716e5512d16
datajake1999 <datajake@gmail.com>
2018-09-02 15:47:06 -0700

    Changed from a while loop to a for loop in the fm_generate_resampled_function.

--- Driver/fmopl3lib/opl3class.cpp
+++ Driver/fmopl3lib/opl3class.cpp
@@ -80,11 +80,10 @@ void opl3class::fm_generate_resampled(signed short *buffer, unsigned int len) {
 	{
 		sample_t ls, rs;
 		unsigned int to_write = resampler_get_min_fill(resampler);
-		while (to_write)
+		for (; to_write--;)
 		{
 			emul.generate(samples, 1);
 			resampler_write_pair(resampler, samples[0], samples[1]);
-			--to_write;
 		}
 		resampler_peek_pair(resampler, &ls, &rs);
 		resampler_read_pair(resampler, &ls, &rs);
