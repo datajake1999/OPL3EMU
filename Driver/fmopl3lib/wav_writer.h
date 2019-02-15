39c2d85505aa5baa90133366897a56eccf37610a
datajake1999 <datajake@gmail.com>
2019-02-15 04:38:38 -0800

    Reordered include statements in wav_writer.h to reduce code size.

--- Driver/fmopl3lib/wav_writer.h
+++ Driver/fmopl3lib/wav_writer.h
@@ -20,8 +20,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#include <windows.h>
 #include <stdio.h>
+#include <windows.h>
 #ifndef WAV_WRITER_H
 #define WAV_WRITER_H
 
