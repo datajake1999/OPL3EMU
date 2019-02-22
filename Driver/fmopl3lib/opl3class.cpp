15b93ee7e1b85dd81138e4852de64ddadc229ab5
datajake1999 <datajake@gmail.com>
2019-02-22 15:25:09 -0800

    Added cross-platform debug code for resampler in opl3class.cpp.

--- Driver/fmopl3lib/opl3class.cpp
+++ Driver/fmopl3lib/opl3class.cpp
@@ -39,7 +39,14 @@ int opl3class::fm_init(unsigned int rate) {
 		{
 			emul.Init(49716);
 #ifdef _DEBUG
+#ifdef _MSC_VER
+#define WIN32
+#endif
+#ifdef WIN32
 			rawsamples = fopen("C:\\OPLSynth\\rawsamples.pcm", "ab");
+#else
+			rawsamples = fopen("rawsamples.pcm", "ab");
+#endif
 #endif
 			resampler = resampler_create();
 			resampler_set_rate(resampler, 49716.0 / (double)rate);
