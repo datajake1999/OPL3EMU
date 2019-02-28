26315b1b6b3466937e5933acd480a575ae48aa2c
datajake1999 <datajake@gmail.com>
2019-02-27 17:59:18 -0800

    Added a C++ port of the Java OPL3 emulator from the JuceOPLVSTi project. Wrapper partially bassed on code from the libADLMIDI project.

---
+++ Driver/zdopl/zdopl_wrapper.cpp
@@ -0,0 +1,48 @@
+//
+// Copyright (C) 2018 Datajake
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+
+#include <string.h>
+#include <math.h>
+#include "zdopl_wrapper.h"
+
+void zdopl_wrapper::Init(unsigned int rate) {
+	chip = JavaOPLCreate(false);
+	chip->Reset();
+}
+
+void zdopl_wrapper::WriteReg(unsigned short reg, unsigned char data) {
+	chip->WriteReg(reg, data);
+}
+
+void zdopl_wrapper::Generate(signed short *buffer, unsigned int len)
+{
+	enum { maxframes = 256 };
+	float buf[2 * maxframes];
+	while(len > 0)
+	{
+		memset(buf, 0, sizeof(buf));
+		unsigned int curframes = (len < (unsigned int)maxframes) ? len : (unsigned int)maxframes;
+		chip->Update(buf, (int)curframes);
+		unsigned int cursamples = 2 * curframes;
+		for(unsigned int i = 0; i < cursamples; ++i)
+		{
+			signed long sample = (signed long)lround(24576 * buf[i]);
+			sample = (sample > -32768) ? sample : -32768;
+			sample = (sample < +32767) ? sample : +32767;
+			buffer[i] = (signed short)sample;
+		}
+		buffer += cursamples;
+		len -= curframes;
+	}
+}
---
+++ Driver/zdopl/zdopl_wrapper.h
@@ -0,0 +1,24 @@
+//
+// Copyright (C) 2018 Datajake
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+
+#include "zdopl.h"
+
+class zdopl_wrapper {
+private:
+	OPLEmul *chip;
+public:
+	void Init(unsigned int rate);
+	void WriteReg(unsigned short reg, unsigned char data);
+	void Generate(signed short *buffer, unsigned int length);
+};
