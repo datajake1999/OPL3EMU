37c53a8f638346b6b6aa90a4cbfbb634f87842d0
datajake1999 <datajake@gmail.com>
2019-02-27 18:10:56 -0800

    Updated mingw scripts.

--- Driver/test/mingw/compile.bat
+++ Driver/test/mingw/compile.bat
@@ -1,1 +1,1 @@
-g++ ..\opl3midi_test.cpp ..\..\fmopl3lib\*.cpp ..\..\opal\*.cpp ..\..\synthlib\opl3midi.cpp -s -static -o opl3midi_test.exe
+g++ ..\opl3midi_test.cpp ..\..\fmopl3lib\*.cpp ..\..\opal\*.cpp ..\..\synthlib\opl3midi.cpp ..\..\zdopl\*.cpp -s -static -o opl3midi_test.exe
